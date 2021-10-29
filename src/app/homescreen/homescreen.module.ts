1a1ecddf21e5197feead61bf235fd057ac6465b9
go-roii <royfgopez80@gmail.com>
2021-10-30 00:41:50 +0800

    fix homescreen ui

--- src/app/homescreen/homescreen.module.ts
+++ src/app/homescreen/homescreen.module.ts
@@ -4,8 +4,8 @@ import { HomescreenRoutingModule } from './homescreen-routing.module';
 import { HomescreenComponent } from './homescreen.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { EventCardComponent } from './event-card/event-card.component';
-import { NewEventCardComponent } from './new-event-card/new-event-card.component';
+import { EventCardComponent } from './create-event/event-card/event-card.component';
+import { NewEventCardComponent } from './create-event/new-event-card/new-event-card.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 
