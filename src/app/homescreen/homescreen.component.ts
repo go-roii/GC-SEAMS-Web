7538a1023c387c66c1906e835d95ca0131042436
go-roii <royfgopez80@gmail.com>
2021-10-31 01:14:07 +0800

    implement active page indicator on homescreen

--- src/app/homescreen/homescreen.component.ts
+++ src/app/homescreen/homescreen.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-homescreen',
   templateUrl: './homescreen.component.html',
   styleUrls: ['./homescreen.component.scss']
 })
+
 export class HomescreenComponent implements OnInit {
 
-  constructor() { }
+  currentPage: string = this.router.url;
+
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(event =>{
+      if (event instanceof NavigationStart){
+         this.routerChangeMethod(event.url);
+      }
+   })
+  }
+
+  routerChangeMethod(url: string){
+    this.currentPage = url;
   }
 
 }
