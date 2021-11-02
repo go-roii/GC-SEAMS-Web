a470a3b5ffbb20696dccba539e3c654723e15774
go-roii <royfgopez80@gmail.com>
2021-11-02 20:04:10 +0800

    clean up code

--- src/app/homescreen/events/event-card/events-event-card.component.ts
+++ src/app/homescreen/events/event-card/events-event-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'events-event-card',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsEventCardComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('className') componentClass: string;
+
+  constructor() {
+    this.componentClass = 'col-xl-3 col-lg-4 col-md-6 col-sm-12';
+   }
 
   ngOnInit(): void {
   }
