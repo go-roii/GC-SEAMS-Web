b6e9f8049e1fa425b197803ebc98603c0efb4ee5
Mark Jason Margallo <jasonmarkollagram@gmail.com>
2021-11-04 17:25:28 +0800

    added missing model

---
+++ src/app/models/Courses.ts
@@ -0,0 +1,11 @@
+export class Courses{
+  course_id:number;
+  course_code:string;
+  course_name:string;
+
+  constructor(){
+    this.course_id=0;
+    this.course_name='';
+    this.course_code='';
+  }
+}
