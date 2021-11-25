32b1f4575d837a4cdcbb58041accaf0c354c6fed
Mark Jason Margallo <jasonmarkollagram@gmail.com>
2021-11-25 14:02:24 +0800

    Implemented past and upcoming events

--- src/app/homescreen/events/ended/ended.component.ts
+++ src/app/homescreen/events/ended/ended.component.ts
@@ -1,5 +1,10 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { SidenavExpandService } from 'src/app/services/sidenav-expand.service';
+import {EventsToAdd} from "../../../models/EventsToAdd";
+import {UserService} from "../../../services/user.service";
+import {DataService} from "../../../services/data.service";
+import {HttpHeaders} from "@angular/common/http";
+import {RequestParams} from "../../../models/RequestParams";
 
 @Component({
   selector: 'app-ended',
@@ -9,19 +14,49 @@ import { SidenavExpandService } from 'src/app/services/sidenav-expand.service';
 
 export class EndedComponent implements OnInit {
 
-  currentItem = 'ended';
+  endedEvents: EventsToAdd[]=[];
   isSidenavExpanded: boolean = this.sidedenavExpandService.isSidenavExpanded;
   @HostBinding('className') componentClass: string = this.isSidenavExpanded ? 'row g-3' : 'row g-4';
-  
-  constructor(private sidedenavExpandService: SidenavExpandService) {
+
+  constructor(private sidedenavExpandService: SidenavExpandService,
+              private userService: UserService,
+              private dataService: DataService) {
     this.sidedenavExpandService.sidenavExpandChange.subscribe((value) => {
       this.isSidenavExpanded = value;
       this.isSidenavExpanded ? this.componentClass = 'row g-3' : this.componentClass = 'row g-4';
     });
   }
 
+  getHttpOptions(){
+    const trimmedHeader=this.userService.getAuthHeader().split(':');
+    const httpOptions = {
 
-  ngOnInit(): void {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: trimmedHeader[1]
+      })
+    };
+    return httpOptions;
   }
 
+  fetchEndedEvents(){
+    const endedEventsParams=new RequestParams();
+    endedEventsParams.EndPoint='/events/past'
+    endedEventsParams.requestType=5;
+    endedEventsParams.authToken=this.getHttpOptions();
+
+    this.dataService.httprequest(endedEventsParams)
+      .subscribe(async (data: EventsToAdd[]) =>{
+        await this.setEndedEvents(data);
+        await console.log(this.endedEvents)
+      });
+  }
+
+  setEndedEvents(data: EventsToAdd[]){
+    this.endedEvents=data;
+  }
+
+  ngOnInit(): void {
+    this.fetchEndedEvents();
+  }
 }
--- src/app/homescreen/events/pending/pending.component.ts
+++ src/app/homescreen/events/pending/pending.component.ts
@@ -1,5 +1,10 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { SidenavExpandService } from 'src/app/services/sidenav-expand.service';
+import {EventsToAdd} from "../../../models/EventsToAdd";
+import {UserService} from "../../../services/user.service";
+import {DataService} from "../../../services/data.service";
+import {HttpHeaders} from "@angular/common/http";
+import {RequestParams} from "../../../models/RequestParams";
 
 @Component({
   selector: 'app-pending',
@@ -9,19 +14,52 @@ import { SidenavExpandService } from 'src/app/services/sidenav-expand.service';
 
 export class PendingComponent implements OnInit {
 
-  //currentItem = 'pending';
+  pendingEvents: EventsToAdd[]=[];
   isSidenavExpanded: boolean = this.sidedenavExpandService.isSidenavExpanded;
   @HostBinding('className') componentClass: string = this.isSidenavExpanded ? 'row g-3' : 'row g-4';
 
-  constructor(private sidedenavExpandService: SidenavExpandService) {
+  constructor(private sidedenavExpandService: SidenavExpandService,
+              private userService: UserService,
+              private dataService: DataService) {
+
     this.sidedenavExpandService.sidenavExpandChange.subscribe((value) => {
       this.isSidenavExpanded = value;
       this.isSidenavExpanded ? this.componentClass = 'row g-3' : this.componentClass = 'row g-4';
     });
   }
 
+  getHttpOptions(){
+    const trimmedHeader=this.userService.getAuthHeader().split(':');
+    const httpOptions = {
+
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: trimmedHeader[1]
+      })
+    };
+    return httpOptions;
+  }
+
+  fetchPendingEvents(){
+    const pendingEventsParams=new RequestParams();
+    pendingEventsParams.EndPoint='/events/upcoming'
+    pendingEventsParams.requestType=5;
+    pendingEventsParams.authToken=this.getHttpOptions();
+
+    this.dataService.httprequest(pendingEventsParams)
+      .subscribe(async (data: EventsToAdd[]) =>{
+        await this.setPendingEvents(data);
+        await console.log(this.pendingEvents)
+      });
+  }
+
+  setPendingEvents(data: EventsToAdd[]){
+    this.pendingEvents=data;
+  }
 
   ngOnInit(): void {
+    this.fetchPendingEvents();
   }
+
 
 }
