ff47b56213ae395c6b9a87388a7d73fb9d84f99c
Mark Jason Margallo <jasonmarkollagram@gmail.com>
2021-11-25 15:04:30 +0800

    Added grouped-vertical-bar-chart component

--- src/app/homescreen/dashboard/dashboard.module.ts
+++ src/app/homescreen/dashboard/dashboard.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { GroupedVerticalBarChartComponent } from './grouped-vertical-bar-chart/grouped-vertical-bar-chart.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    GroupedVerticalBarChartComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/homescreen/dashboard/grouped-vertical-bar-chart/grouped-vertical-bar-chart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-grouped-vertical-bar-chart',
+  templateUrl: './grouped-vertical-bar-chart.component.html',
+  styleUrls: ['./grouped-vertical-bar-chart.component.scss']
+})
+export class GroupedVerticalBarChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
