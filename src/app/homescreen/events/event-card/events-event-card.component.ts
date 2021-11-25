1319279e9c0ef7eecf7a01f23cba0d409badcf35
go-roii <royfgopez80@gmail.com>
2021-11-25 19:02:02 +0800

    add seminar hours to event card

--- src/app/homescreen/events/event-card/events-event-card.component.ts
+++ src/app/homescreen/events/event-card/events-event-card.component.ts
@@ -18,6 +18,8 @@ export class EventsEventCardComponent implements OnInit {
   eventCurrentTime!: number;
   currentProgress!: number;
 
+  seminarHours!: string;
+
   constructor() {}
 
 
@@ -43,6 +45,8 @@ export class EventsEventCardComponent implements OnInit {
     else
       this.currentProgress = 100
 
+    this.seminarHours = this.getSeminarHours(this.eventDurationTime)
+
     // if(this.currentProgress > 0 && this.currentProgress != 100)
     //   setInterval(() => {
     //     this.currentDate = new Date;
@@ -60,6 +64,12 @@ export class EventsEventCardComponent implements OnInit {
 
     console.log('event duration: ' + this.eventDurationTime + 'min')
     console.log('event progress: ' + this.currentProgress + '%')
+  }
+
+  getSeminarHours(num: number) {
+    var hours = (num / 60)
+
+    return (num / 60).toFixed(1) + ' hr'
   }
 
 }
